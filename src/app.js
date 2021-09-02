de605fedcfa1929483bbbd3cb0824aee5a72363a
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 17:05:10 -0300

    feat: userRoute

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const { sequelize } = require("./model");
 const pg = require("pgtools");
 const postRoute = require("./routes/post");
+const userRoute = require("./routes/user");
 
 
 const app = express();
 
 app.use(express.json());
 app.use("/post", postRoute);
+app.use("/user", userRoute);
 sequelize.sync().then(() => console.log("connected"));
 
 app.listen(4000, () => {
