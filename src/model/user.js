0d178ff4e8d83df1701ce1091b2df7bbf402ca29
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 17:07:13 -0300

    feat: user model

---
+++ src/model/user.js
@@ -0,0 +1,20 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    confirmPassword: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+  });
+
+  return User;
+};
+
+module.exports = user;
