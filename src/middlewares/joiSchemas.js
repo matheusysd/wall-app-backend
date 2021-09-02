f03cdd30e1121968b1d4fda37839db8040615b73
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 17:06:09 -0300

    feat: joiSchemas validation

---
+++ src/middlewares/joiSchemas.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+const schemas = {
+  userPOST: Joi.object().keys({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email({ tlds: false }).required(),
+    password: Joi.string().min(6).max(30).required(),
+  }),
+  userLoginPOST: Joi.object().keys({
+    email: Joi.string().email({ tlds: false }).required(),
+    password: Joi.string().min(6).max(30).required(),
+  }),
+  createEditPostPOST: Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    author: Joi.string().required(),
+  }),
+  postParams: Joi.object({
+    id: Joi.string().required(),
+  }),
+};
+
+module.exports = schemas;
