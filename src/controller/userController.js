2458e2a272387461a8b5e3b5b33c9e4b46666c80
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 19:16:53 -0300

    feat: user controller
    create and login

---
+++ src/controller/userController.js
@@ -0,0 +1,61 @@
+const bcrypt = require("bcryptjs");
+const { userModel } = require("../model");
+const { generateToken } = require("../service/jwtAuthentication");
+
+const BCRYPT_SALT = bcrypt.genSaltSync(10); //add .env later
+
+exports.post = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await userModel.findOne({ where: { email } });
+
+  if (userExists) {
+    return res.status(409).json({
+      error: "User already exists!",
+    });
+  }
+
+  await userModel.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, BCRYPT_SALT),
+  });
+
+  return res.status(201).json({
+    message: "User registered with success!",
+  });
+  
+};
+
+exports.get = async (req, res) => {
+  const users = await userModel.findAll();
+  res.json(users);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Unauthorized, wrong email!",
+    });
+  }
+  const authUser = await bcrypt.compare(password, user.password);
+
+  if (!authUser) {
+    return res.status(401).json({
+      message: "Unauthorized, wrong password",
+    });
+  }
+
+  const userPayload = {
+    userId: user.dataValues.id,
+    name: user.name,
+  };
+
+  const token = generateToken(userPayload);
+
+  return res.status(200).json({ token, ...userPayload });
+
+};
