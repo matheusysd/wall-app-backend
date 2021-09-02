8ba125b1354bbf376740c434a90b9009fffb5689
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 19:27:50 -0300

    create base post routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/postController");
+const joiSchemas = require("../middlewares/joiSchemas");
+const joiMiddleware = require("../middlewares/joiMiddleware");
+const { validateJWTToken } = require("../service/jwtAuthentication");
 
 router.get("/", controller.get);
-router.post("/", controller.post);
+router.post("/", validateJWTToken, joiMiddleware(joiSchemas.createEditPostPOST, "body"), controller.post);
 
 module.exports = router;
