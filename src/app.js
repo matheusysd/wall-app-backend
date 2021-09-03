c945d4c1898546b32e303dfd337edded8dca8233
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-03 00:05:54 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const { sequelize } = require("./model");
 const pg = require("pgtools");
+const cors = require("cors");
+
 const postRoute = require("./routes/post");
 const userRoute = require("./routes/user");
 
-
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/post", postRoute);
 app.use("/user", userRoute);
