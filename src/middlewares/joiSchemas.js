4a6b4c255099b728872d225698a51f7db26f40b5
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-03 00:04:57 -0300

    add last name ai joi schema

--- src/middlewares/joiSchemas.js
+++ src/middlewares/joiSchemas.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const schemas = {
   userPOST: Joi.object().keys({
     name: Joi.string().min(3).max(50).required(),
+    lastName: Joi.string().min(3).max(50).required(),
     email: Joi.string().email({ tlds: false }).required(),
     password: Joi.string().min(6).max(30).required(),
   }),
