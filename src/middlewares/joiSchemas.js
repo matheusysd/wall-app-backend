854fb0d65ece1b9527b1dd534a021266561209e2
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-05 02:55:52 -0300

    add userId at userPost schema

--- src/middlewares/joiSchemas.js
+++ src/middlewares/joiSchemas.js
@@ -4,6 +4,7 @@ const schemas = {
   userPOST: Joi.object().keys({
     name: Joi.string().min(3).max(50).required(),
     lastName: Joi.string().min(3).max(50).required(),
+    userId: Joi.string().min(3).max(50),
     email: Joi.string().email({ tlds: false }).required(),
     password: Joi.string().min(6).max(30).required(),
   }),
