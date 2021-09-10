2df11f608f4780f96557796e74f13a78fcc8a9d6
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-10 02:23:22 -0300

    remove db file

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  dialect: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: process.env.DATABASE,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-};
