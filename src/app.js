ca804b53c86f84388d37af0b500070cf99c4f137
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-10 01:49:20 -0300

    removee .listen

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
+require("dotenv/config");
 const express = require("express");
-const { sequelize } = require("./model");
-const pg = require("pgtools");
 const cors = require("cors");
 
 const postRoute = require("./routes/post");
@@ -12,8 +10,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/post", postRoute);
 app.use("/user", userRoute);
-sequelize.sync().then(() => console.log("connected"));
 
-app.listen(4000, () => {
-  console.log("app is online");
-});
+module.exports = app;
