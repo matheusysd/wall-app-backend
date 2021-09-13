fadd07051c18e77a1108b38e22098074e1ec93ac
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-13 18:21:45 -0300

    try to connect postgres at heroku

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 const config = require("./config");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { native: true });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 module.exports = sequelize;
