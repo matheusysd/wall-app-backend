ef750dc3552ee2c97d937dd1249fa5240046f3cf
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-13 18:19:08 -0300

    change ssl option to true on sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 const config = require("./config");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, { native: true });
 
 module.exports = sequelize;
