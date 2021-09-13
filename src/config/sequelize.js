674acfc242b47303623b9cd174ad5b3b5039a85c
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-13 18:13:36 -0300

    add DB_URI to env

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 const config = require("./config");
 
-const sequelize = new Sequelize(config[process.env.APP_ENVIRONMENT || "development"]);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
