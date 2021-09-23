dddce6746b806cc812c8efd7d04e0401e7708d08
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-22 21:43:48 -0300

    remove comments from merge

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,19 +1,6 @@
 const Sequelize = require("sequelize");
 const config = require("./config");
 
-<<<<<<< HEAD
 const sequelize = new Sequelize(config);
-=======
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
->>>>>>> 2538a195836736884cdbf5d643bf30dab5a52504
 
 module.exports = sequelize;
